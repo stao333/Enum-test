a23ca435b4a60cb1b55ed44ff5af777adad8ad75
stao333 <stao333@gmail.com>
2017-03-27 00:12:49 +0100

    this is the initial commit of a sample project.

---
+++ src/ActivityType.java
@@ -0,0 +1,24 @@
+public enum ActivityType
+{
+	TYPE1("type1"), TYPE2("type2"), TYPE3("type3");
+
+	private String typeCode;
+
+	private ActivityType( String typeCode ) {
+		this.typeCode = typeCode;
+	}
+
+	public static ActivityType getTypeByCode( String code ) {
+		switch( code ) {
+			case "type1":
+				return ActivityType.TYPE1;
+			case "type2":
+				return ActivityType.TYPE2;
+			case "type3":
+				return ActivityType.TYPE3;
+			default:
+				throw new RuntimeException();
+		}
+
+	}
+}
---
+++ src/EnumTest.java
@@ -0,0 +1,20 @@
+public class EnumTest
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main( String[] args ) {
+		ActivityType at1 = ActivityType.getTypeByCode( "type1" );
+		ActivityType at2 = ActivityType.getTypeByCode( "type2" );
+		ActivityType at3 = ActivityType.getTypeByCode( "type3" );
+
+		System.out.println( at1.toString() );
+		System.out.println( at2.toString() );
+		System.out.println( at3.toString() );
+
+		ActivityType at4 = ActivityType.getTypeByCode( "type4" );
+		System.out.println( at4.toString() );
+	}
+
+}
