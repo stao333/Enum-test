3fd39d9b2bab213243f596295ea4214cd3ce4c14
stao333 <stao333@gmail.com>
2017-03-27 00:27:55 +0100

    I just changed EnumTest.java

--- src/EnumTest.java
+++ src/EnumTest.java
@@ -13,6 +13,7 @@ public class EnumTest
 		System.out.println( at2.toString() );
 		System.out.println( at3.toString() );
 
+		// some comment.
 		ActivityType at4 = ActivityType.getTypeByCode( "type4" );
 		System.out.println( at4.toString() );
 	}
